59c1ef877721d482e7d2eae20629078e14748e7f
María Torrente <caritotorrente@gmail.com>
2021-03-09 08:33:48 -0500

    Pseudocode written

---
+++ app.js
@@ -0,0 +1,17 @@
+// Require https module to get the object from the fetching data.
+const https = require("https");
+
+// Require xml2js module
+const parser = require("xml2js");
+
+// Make the get request I should need to passed the url and a callback function for the response.
+
+// Be sure to return an obj
+
+// Add the data
+
+// Parse XML and create newObj format.
+
+// Loop the Obj to extract the needed data for the new format.
+
+// Handle errors.
